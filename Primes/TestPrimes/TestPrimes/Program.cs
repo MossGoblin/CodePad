1f09919658703af5253cd5bb49c3292a1788aa9c
MossGoblin <hugolabs@gmail.com>
2019-11-12 09:42:47 +0200

    [Initial] + PathPrimes - an attempts at a class library, providing some basic prime number checks and perhaps operations

---
+++ Primes/TestPrimes/TestPrimes/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathPrimes;
+
+
+namespace TestPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathP primath = new MathP();
+
+            //Console.WriteLine("- - - - - - -");
+            //Console.WriteLine("1. Test if a number is prime");
+            //Console.Write("Number to test: ");
+            //ulong testNumber = ulong.Parse(Console.ReadLine());
+            //Console.WriteLine($"{testNumber} result: {primath.IsPrime(testNumber)}");
+            //Console.WriteLine($"collected {primath.GetCached().Count} :: {PrintCollected(primath.GetCached())}");
+
+
+            //Console.WriteLine("- - - - - - -");
+            //Console.WriteLine("2. Number of primes up to a number");
+            //Console.Write("Upper bound (inclusive): ");
+            //ulong upper = ulong.Parse(Console.ReadLine());
+            //Console.WriteLine($"up to {upper} result: {PrintList(primath.PrimesBelow(upper))}");
+            //Console.WriteLine($"collected {primath.GetCached().Count} :: {PrintCollected(primath.GetCached())}");
+
+            //Console.WriteLine("- - - - - - -");
+            //Console.WriteLine("3. Number of primes in a range");
+            //Console.Write("Lower bound: ");
+            //ulong lower = ulong.Parse(Console.ReadLine());
+            //Console.Write("Upper bound: ");
+            //upper = ulong.Parse(Console.ReadLine());
+            //Console.WriteLine($"{lower}..{upper} result: {PrintList(primath.PrimesInRange(lower, upper))}");
+            //Console.WriteLine($"collected {primath.GetCached().Count} :: {PrintCollected(primath.GetCached())}");
+
+            Console.WriteLine("- - - - - - -");
+            Console.WriteLine("4. Factors of a number");
+            Console.Write("Number: ");
+            ulong target = ulong.Parse(Console.ReadLine());
+            Console.WriteLine($"{target} result: {PrintList(primath.Factors(target))}");
+
+            Console.WriteLine("- - - - - - -");
+            Console.WriteLine("5. Prime factors of a number");
+            Console.Write("Number: ");
+            target = ulong.Parse(Console.ReadLine());
+            Console.WriteLine($"{target} result: {PrintList(primath.PrimeFactors(target))}");
+
+        }
+
+        public static string PrintList(List<ulong> list)
+        {
+            return String.Join(", ", list);
+        }
+
+        public static string PrintCollected(SortedDictionary<ulong, bool> dict)
+        {
+            List<ulong> list = new List<ulong>();
+            foreach (var item in dict)
+            {
+                if (item.Value)
+                {
+                    list.Add(item.Key);
+                }
+            }
+
+            return String.Join(", ", list.OrderBy(n => n));
+        }
+    }
+}
---
+++ temporee/temporee/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace temporee
+{
+    public class Program
+    {
+        static public void Main(string[] args)
+        {
+
+            int count = 0;
+            int a = 7;
+            int b = 9;
+            int c = 3;
+            int d = 5;
+
+            Console.WriteLine("---");
+            Console.WriteLine("Iterate");
+            Console.WriteLine();
+            foreach (var step in Griderator.Iterate(a, b))
+            {
+                Console.WriteLine($"(count {count} / calc {step.Item1 * b + step.Item2}) : {step.Item1} / {step.Item2}");
+                count++;
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine("Iterate range");
+            Console.WriteLine();
+            count = 0;
+            foreach (var step in Griderator.IterateRange(c, a, d, b))
+            {
+                Console.WriteLine($"(count {count} / calc {step.Item1 * b + step.Item2}) : {step.Item1} / {step.Item2}");
+                count++;
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine("Iterate With Struct");
+            Console.WriteLine();
+            count = 0;
+            foreach (var step in Griderator.IterateWithStruct(c, d))
+            {
+                Console.WriteLine($"(count {count} / calc {step.outer * b + step.inner}) : {step.outer} / {step.inner}");
+                count++;
+            }
+
+            Console.WriteLine("---");
+            Console.WriteLine("Iterate with Count");
+            Console.WriteLine();
+            count = 0;
+            foreach (var step in Griderator.IterateWithCount(a, b))
+            {
+                Console.WriteLine($"(count {count} / calc {step.c}) : {step.i} / {step.y}");
+                count++;
+            }
+        }
+
+    }
+
+    public class Griderator
+    {
+        public struct Step
+        {
+            public int outer;
+            public int inner;
+
+            public Step(int o, int i)
+            {
+                outer = o;
+                inner = i;
+            }
+        }
+        static public IEnumerable<Step> IterateWithStruct(int bound_i, int bound_b)
+        {
+
+            List<Step> loop = new List<Step>();
+            for (int i = 0; i < bound_i; i++)
+            {
+                for (int y = 0; y < bound_b; y++)
+                {
+                    Step newStep = new Step(i, y);
+                    loop.Add(newStep);
+                }
+            }
+
+            return loop;
+        }
+
+        static public IEnumerable<(int i, int y)> Iterate(int bound_i, int bound_b)
+        {
+            List<(int i, int y)> loop = new List<(int i, int y)>();
+            for (int i = 0; i < bound_i; i++)
+            {
+                for (int y = 0; y < bound_b; y++)
+                {
+                    loop.Add((i, y));
+                }
+            }
+
+            return loop;
+        }
+
+        static public IEnumerable<(int i, int y, int c)> IterateWithCount(int bound_i, int bound_b)
+        {
+            List<(int i, int y, int c)> loop = new List<(int i, int y, int c)>();
+            int count = 0;
+            for (int i = 0; i < bound_i; i++)
+            {
+                for (int y = 0; y < bound_b; y++)
+                {
+                    loop.Add((i, y, count));
+                    count++;
+                }
+            }
+
+            return loop;
+        }
+
+        static public IEnumerable<(int i, int y)> IterateRange(int lower_i, int higher_i, int lower_y, int higher_b)
+        {
+            List<(int i, int y)> loop = new List<(int i, int y)>();
+            for (int i = lower_i; i < higher_i; i++)
+            {
+                for (int y = lower_y; y < higher_b; y++)
+                {
+                    loop.Add((i, y));
+                }
+            }
+
+            return loop;
+        }
+    }
+}
+
