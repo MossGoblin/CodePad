2b9440db248e9cecfda57e9a915cac3c92dc041c
MossGoblin <hugolabs@gmail.com>
2019-11-12 14:42:35 +0200

    + Compartment Collator - [specific] room compiler that should be able to assess a number of rooms and return a list of rooms that follows pre-set requirements for averages and sums for the entire collection

---
+++ RoomCompiler/RoomCompiler/CompartmentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoomCompiler
+{
+    enum CompartmentType
+    {
+        Connection = 0, // only connects other compartments
+        Storage = 2, // used to store movables
+        Utility = 3, // used to enable or enhance actions
+        Common = 4, // used to allow gathering of people
+        Personal = 5 // used as personal space for single people or small groups
+    }
+}
---
+++ RoomCompiler/RoomCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoomCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
