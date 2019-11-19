4b0064114cc49b7711dab99e850eb2d4fbbb63a2
MossGoblin <hugolabs@gmail.com>
2019-11-19 16:34:16 +0200

    ~ Eventer  testing some more delegate stuff

--- Eventer/Eventer/Eventer/Program.cs
+++ Eventer/Eventer/Eventer/Program.cs
@@ -8,7 +8,9 @@ namespace Eventer
     {
         static void Main(string[] args)
         {
-            List<Subject> sbjList = new List<Subject>();
+
+            // pass 1
+            //List<Subject> sbjList = new List<Subject>();
             //var sbj = New<Subject>.Instance();
             //Console.WriteLine(sbj.ToString());
 
@@ -16,23 +18,42 @@ namespace Eventer
             //string result = pbl.GetSbjString(sbj, "TESTING");
             //Console.WriteLine(result);
 
-            Random rnd = new Random();
-            Publisher pbl = new Publisher();
+            // pass 2
+            //List<Subject> sbjList = new List<Subject>();
+            //Random rnd = new Random();
+            //Publisher pbl = new Publisher();
 
-            for (int count = 0; count < 10; count++)
-            {
-                Subject newSubject = pbl.GenNewSbj(rnd.Next());
-                sbjList.Add(newSubject);
-                Console.WriteLine(newSubject.ToString());
-            }
+            //for (int count = 0; count < 10; count++)
+            //{
+            //    Subject newSubject = pbl.GenNewSbj(rnd.Next());
+            //    sbjList.Add(newSubject);
+            //    Console.WriteLine(newSubject.ToString());
+            //}
 
-            foreach (Subject sbj in sbjList)
-            {
-                Console.WriteLine(pbl.GetSbjString(sbj, "Acounted for: "));
-            }
+            //foreach (Subject sbj in sbjList)
+            //{
+            //    Console.WriteLine(pbl.GetSbjString(sbj, "Acounted for: "));
+            //}
+
+            // pass 3
+            SomeClass some = new SomeClass();
+            some.WorkOnThings(10, ReportingToConsole);
+            Console.WriteLine();
+            some.WorkOnThings(5, ReportingToConsoleToo);
+
 
 
         }
+        public static void ReportingToConsole(int progress)
+        {
+            Console.WriteLine("current process " + progress);
+        }
+
+        public static void ReportingToConsoleToo(int progress)
+        {
+            Console.WriteLine("progress is >> " + progress);
+        }
+
     }
 
     public class New<T>
