08045d20bc72ce9bfd5da30c0ee730e07a6c74f2
MossGoblin <hugolabs@gmail.com>
2019-11-19 11:20:41 +0200

    ~ Eventer - sommore messing around

--- Eventer/Eventer/Eventer/Program.cs
+++ Eventer/Eventer/Eventer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace Eventer
@@ -7,12 +8,30 @@ namespace Eventer
     {
         static void Main(string[] args)
         {
-            var sbj = New<Subject>.Instance();
-            Console.WriteLine(sbj.ToString());
+            List<Subject> sbjList = new List<Subject>();
+            //var sbj = New<Subject>.Instance();
+            //Console.WriteLine(sbj.ToString());
 
+            //Publisher pbl = new Publisher();
+            //string result = pbl.GetSbjString(sbj, "TESTING");
+            //Console.WriteLine(result);
+
+            Random rnd = new Random();
             Publisher pbl = new Publisher();
-            string result = pbl.GetSbjString(sbj, "TESTING");
-            Console.WriteLine(result);
+
+            for (int count = 0; count < 10; count++)
+            {
+                Subject newSubject = pbl.GenNewSbj(rnd.Next());
+                sbjList.Add(newSubject);
+                Console.WriteLine(newSubject.ToString());
+            }
+
+            foreach (Subject sbj in sbjList)
+            {
+                Console.WriteLine(pbl.GetSbjString(sbj, "Acounted for: "));
+            }
+
+
         }
     }
 
--- Eventer/Eventer/Eventer/Publisher.cs
+++ Eventer/Eventer/Eventer/Publisher.cs
@@ -4,20 +4,30 @@ using System.Text;
 
 namespace Eventer
 {
-    public delegate string GenDel(Subject sbj, string str);
+    public delegate string StringExtract(Subject sbj, string str);
+    public delegate Subject SubjectGenerator(int ID);
 
     class Publisher
     {
-        public event GenDel GenEvent;
+        public event StringExtract ExtractStrings;
+        public event SubjectGenerator GenerateSubject;
 
         public Publisher()
         {
-            this.GenEvent += new GenDel(GetSbjString);
+            this.GenerateSubject += new SubjectGenerator(GenNewSbj);
+            this.ExtractStrings += new StringExtract(GetSbjString);
         }
 
         public string GetSbjString(Subject sbj, string str)
         {
             return ($"{str} + {sbj.ToString()}");
         }
+
+        public Subject GenNewSbj(int ID)
+        {
+            Subject sbj = new Subject(ID);
+            return sbj;
+        }
+
     }
 }
