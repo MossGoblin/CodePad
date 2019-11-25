6e42badd08903cd25f1de5da8b17d61cd120e4a3
MossGoblin <hugolabs@gmail.com>
2019-11-25 20:52:57 +0200

    [m] ~ Weighted - reduced the class to two methors - normal distribution and population, both taking in double ~ ToolBox - added Weignted.cs; still no documentation in sight...

---
+++ ToolTestRoom/ToolTest/ToolTest/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using ToolBox;
+
+namespace ToolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 1000000;
+            int testNumber = 1;
+
+            List<double> distEven = new List<double>() { 1, 1, 1, 1, 1, 1 };
+            List<double> distCurve = new List<double>() { 1, 2, 4, 5, 4, 2 };
+
+            switch (testNumber)
+            {
+                case 1:
+                    TestRandom("Even", distEven, iterations);
+                    TestRandom("Curved", distCurve, iterations);
+                    break;
+                case 2:
+                    TestRandomReverse("Reverse Even", distEven, iterations);
+                    TestRandomReverse("Reverse Curved", distCurve, iterations);
+                    break;
+                default:
+                    System.Console.WriteLine("No such test");
+                    break;
+            }
+        }
+
+        private static void TestRandom(string display, List<double> dist, int iterations)
+        {
+            // Random test
+
+            double[] collectorINT = new double[dist.Count];
+            Console.WriteLine("\n- - -");
+            Console.WriteLine($">> {display} {String.Join(", ", dist)}");
+            for (int i = 0; i < iterations; i++)
+            {
+                double result = Weighted.Random(dist);
+                collectorINT[(int)result]++;
+            }
+            Console.WriteLine(String.Join(", ", collectorINT));
+        }
+
+        private static void TestRandomReverse(string display, List<double> pop, int iterations)
+        {
+            // Random Reverse test
+
+            int[] collectorINT = new int[pop.Count];
+            Console.WriteLine("\n- - -");
+            Console.WriteLine($">> {display} {String.Join(", ", pop)}");
+            for (int i = 0; i < iterations; i++)
+            {
+                int result = Weighted.RandomReverse(pop);
+                collectorINT[result]++;
+            }
+
+            // Eyeball test
+            int lowest = int.MaxValue;
+            foreach (var item in collectorINT)
+            {
+                lowest = Math.Min(lowest, item);
+            }
+
+            List<double> redacted = new List<double>();
+            int counter = 0;
+            foreach (var item in collectorINT)
+            {
+                redacted.Add(item / lowest + pop[counter]);
+                counter++;
+            }
+
+            Console.WriteLine(String.Join(", ", collectorINT));
+            Console.WriteLine(String.Join(", ", redacted));
+        }
+    }
+}
--- WeightedRandom/WeightedRandom/Program.cs
+++ WeightedRandom/WeightedRandom/Program.cs
@@ -11,125 +11,69 @@ namespace WeightedRandom
             int iterations = 1000000;
             int testNumber = 2;
 
-            List<int> distIntEven = new List<int>() { 1, 1, 1, 1, 1, 1 };
-            List<int> distIntCurve = new List<int>() { 1, 2, 3, 4, 3, 2 };
-
-            List<double> distNRMEven = new List<double>() { 1, 1, 1, 1, 1, 1 };
-            List<double> distNRMCurve = new List<double>() { 0.1, 0.2, 0.4, 0.5, 0.4, 0.2 };
+            List<double> distEven = new List<double>() { 1, 1, 1, 1, 1, 1 };
+            List<double> distCurve = new List<double>() { 1, 2, 4, 5, 4, 2 };
 
             switch (testNumber)
             {
                 case 1:
-                    TestRandom("INT Even", distIntEven, iterations);
-                    TestRandom("INT Curved", distIntCurve, iterations);
+                    TestRandom("Even", distEven, iterations);
+                    TestRandom("Curved", distCurve, iterations);
                 break;
                 case 2:
-                    TestRandomNormal("Normal Even", distNRMEven, iterations);
-                    TestRandomNormal("Normal Curved", distNRMCurve, iterations);
-                break;
-                case 3:
-                    TestRandomReverse("Reverse Int Even", distIntEven, iterations);
-                    TestRandomReverse("Reverse Int Curved", distIntCurve, iterations);
-                break;
-                case 4:
-                    TestRandomNormalReverse("Reverse Normal Even", distNRMEven, iterations);
-                    TestRandomNormalReverse("Reverse Normal Curved", distNRMCurve, iterations);
-                break;
+                    TestRandomReverse("Reverse Even", distEven, iterations);
+                    TestRandomReverse("Reverse Curved", distCurve, iterations);
+                    break;
                 default:
                     System.Console.WriteLine("No such test");
                 break;
             }
         }
 
-        private static void TestRandom(string display, List<int> dist, int iterations)
+        private static void TestRandom(string display, List<double> dist, int iterations)
         {
             // Random test
 
-            int[] collectorINT = new int[dist.Count];
+            double[] collectorINT = new double[dist.Count];
             Console.WriteLine("\n- - -");
             Console.WriteLine($">> {display} {String.Join(", ", dist)}");
             for (int i = 0; i < iterations; i++)
             {
-                int result = Weighted.Random(dist);
-                collectorINT[result] ++;
+                double result = Weighted.Random(dist);
+                collectorINT[(int)result] ++;
             }
             Console.WriteLine(String.Join(", ", collectorINT));
         }
 
-        private static void TestRandomNormal(string display, List<double> dist, int iterations)
-        {
-            // RandomNormal test
-
-            double[] collectorNRM = new double[dist.Count];
-            Console.WriteLine("\n- - -");
-            Console.WriteLine($">> {display} {String.Join(", ", dist)}");
-            for (int i = 0; i < iterations; i++)
-            {
-                int result = Weighted.RandomNormal(dist);
-                collectorNRM[result] ++;
-            }
-            Console.WriteLine(String.Join(", ", collectorNRM));
-        }
-
-        private static void TestRandomReverse(string display, List<int> dist, int iterations)
+        private static void TestRandomReverse(string display, List<double> pop, int iterations)
         {
             // Random Reverse test
 
-            int[] collectorINT = new int[dist.Count];
+            int[] collectorINT = new int[pop.Count];
             Console.WriteLine("\n- - -");
-            Console.WriteLine($">> {display} {String.Join(", ", dist)}");
+            Console.WriteLine($">> {display} {String.Join(", ", pop)}");
             for (int i = 0; i < iterations; i++)
             {
-                int result = Weighted.RandomReverse(dist);
-                collectorINT[result] ++;
+                int result = Weighted.RandomReverse(pop);
+                collectorINT[result]++;
             }
 
             // Eyeball test
             int lowest = int.MaxValue;
-             foreach (var item in collectorINT)
-            {
-                lowest = Math.Min(lowest, item);
-            }
-
-            List<int> redacted = new List<int>();
-            int counter = 0;
             foreach (var item in collectorINT)
-            {
-                redacted.Add(item/lowest + dist[counter]);
-                counter++;
-            }
-
-            Console.WriteLine(String.Join(", ", collectorINT));
-            Console.WriteLine(String.Join(", ", redacted));
-        }
-
-        private static void TestRandomNormalReverse(string display, List<double> dist, int iterations)
-        {
-            // RandomNormal test
-
-            double[] collectorNRM = new double[dist.Count];
-            Console.WriteLine("\n- - -");
-            Console.WriteLine($">> {display} {String.Join(", ", dist)}");
-            for (int i = 0; i < iterations; i++)
-            {
-                int result = Weighted.RandomNormalReverse(dist);
-                collectorNRM[result] ++;
-            }
-
-            // Eyeball test
-            double lowest = double.MaxValue;
-             foreach (var item in collectorNRM)
             {
                 lowest = Math.Min(lowest, item);
             }
 
             List<double> redacted = new List<double>();
-            foreach (var item in collectorNRM)
+            int counter = 0;
+            foreach (var item in collectorINT)
             {
-                redacted.Add(item/(lowest+1));
+                redacted.Add(item / lowest + pop[counter]);
+                counter++;
             }
 
-            Console.WriteLine(String.Join(", ", collectorNRM));
+            Console.WriteLine(String.Join(", ", collectorINT));
             Console.WriteLine(String.Join(", ", redacted));
         }
     }
