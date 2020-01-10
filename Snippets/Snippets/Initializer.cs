791d3dcfb9273a5c3edae9b866c9c38638604dd5
MossGoblin <hugolabs@gmail.com>
2020-01-10 11:18:32 +0200

    [w > snippet] testing collection initializer; when approved should move it to the toolbox

---
+++ Snippets/Snippets/Initializer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snippets
+{
+    static class Initializer
+    {
+        public static IEnumerable<int> Init(int size, int value)
+        {
+            List<int> initializedCollection = new List<int>();
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(value);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<double> Init(int size, double value)
+        {
+            List<double> initializedCollection = new List<double>();
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(value);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<int> InitIterative(int size, int startValue, int step)
+        {
+            List<int> initializedCollection = new List<int>();
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + step * counter);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<int> InitIterative(int size, int startValue, int step, bool descending)
+        {
+            List<int> initializedCollection = new List<int>();
+            int descendingFactor = descending ? -1 : 1;
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + step * counter * descendingFactor);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<int> InitIterative(int size, int startValue)
+        {
+            List<int> initializedCollection = new List<int>();
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + counter);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<int> InitIterative(int size, int startValue, bool descending)
+        {
+            List<int> initializedCollection = new List<int>();
+            int descendingFactor = descending ? -1 : 1;
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + counter * descendingFactor);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<double> InitIterative(int size, double startValue, double step)
+        {
+            List<double> initializedCollection = new List<double>();
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + step * counter);
+            }
+
+            return initializedCollection;
+        }
+
+        public static IEnumerable<double> InitIterative(int size, double startValue, double step, bool descending)
+        {
+            List<double> initializedCollection = new List<double>();
+            int descendingFactor = descending ? -1 : 1;
+            for (int counter = 0; counter < size; counter++)
+            {
+                initializedCollection.Add(startValue + step * counter * descendingFactor);
+            }
+
+            return initializedCollection;
+        }
+    }
+}
--- Snippets/Snippets/Program.cs
+++ Snippets/Snippets/Program.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Snippets
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //CheckBalance();
+            InitArray();
+        }
+
+        private static void InitArray()
+        {
+            int collectionsCount = 5;
+            int initValueInt = 3;
+            double initValueDouble = 3.4;
+            Console.WriteLine(String.Join(", ", Initializer.Init(collectionsCount, initValueInt)));
+            Console.WriteLine(String.Join(", ", Initializer.Init(collectionsCount, initValueDouble)));
+            Console.WriteLine(String.Join(", ", Initializer.InitIterative(collectionsCount, initValueInt)));
+            Console.WriteLine(String.Join(", ", Initializer.InitIterative(collectionsCount, initValueDouble, initValueDouble)));
+            Console.WriteLine(String.Join(", ", Initializer.InitIterative(collectionsCount, 22, 4, true)));
+            Console.WriteLine(String.Join(", ", Initializer.InitIterative(collectionsCount, 182.4, 13.7, true)));
+            double[] resultArray = Initializer.Init(collectionsCount, initValueDouble).ToArray();
+            List<int> resultList = Initializer.Init(collectionsCount, initValueInt).ToList();
+            Console.WriteLine($"Array: {String.Join(" . ", resultArray)}");
+            Console.WriteLine($" List: {String.Join(" . ", resultList)}");
+        }
+
+        private static void CheckBalance()
         {
             Console.WriteLine("Enter states:");
             string input = Console.ReadLine();
